841eb03299870ca81719d21b56cc31bfe78177a6
Nikhil Barot <nikhilbarot@192.168.1.7>
2024-11-15 16:20:40 +0530

    properly-imported

--- src/helper/constants.jsx
+++ src/helper/constants.jsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-export const process = import.meta;
-export const ENV = process.env.VITE_ENV || 'dev';
-export const STRIPE_PUBLISHABLE_KEY = process.VITE_STRIPE_PUBLISHABLE_KEY;
-export const VITE_DOMAIN = process.env.VITE_DOMAIN;
-export const VITE_ACCESS_TOKEN = process.env.VITE_ACCESS_TOKEN;
+export const ENV = import.meta.env.VITE_ENV;
+export const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
+export const VITE_DOMAIN = import.meta.env.VITE_DOMAIN;
+export const VITE_ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 
 export const PAGE_URL = axios.create({
-    baseURL: process.env.VITE_PAGE_URL
+    baseURL: import.meta.env.VITE_PAGE_URL
 })
 
 export const SERVER_URL = axios.create({
-    baseURL: ENV === 'dev' ? process.env.VITE_DEV_SERVER_URL : process.env.VITE_PROD_SERVER_URL
+    baseURL: ENV === 'dev' ? import.meta.env.VITE_DEV_SERVER_URL : import.meta.env.VITE_PROD_SERVER_URL
 })
