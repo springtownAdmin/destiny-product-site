3cf2cb202c27ef685eef5ce8c57d9650e8193d8b
Nikhil Barot <nikhilbarot@192.168.1.7>
2024-11-15 15:51:40 +0530

    shopifyClient.js-updated

--- src/helper/constants.jsx
+++ src/helper/constants.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const process = import.meta;
 export const ENV = process.env.VITE_ENV || 'dev';
-export const STRIPE_PUBLISHABLE_KEY = process.VITE_STRIPE_PUBLISHABLE_KEY
+export const STRIPE_PUBLISHABLE_KEY = process.VITE_STRIPE_PUBLISHABLE_KEY;
+export const VITE_DOMAIN = process.env.VITE_DOMAIN;
+export const VITE_ACCESS_TOKEN = process.env.VITE_ACCESS_TOKEN;
 
 export const PAGE_URL = axios.create({
     baseURL: process.env.VITE_PAGE_URL
